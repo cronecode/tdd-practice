204b5593d1dbeca932f10dc02888f436b0063eb6
kborn <kristina.born@gmail.com>
2017-05-17 10:09:33 -0400

    implements subtraction

--- wordy/wordy.rb
+++ wordy/wordy.rb
@@ -1,6 +1,9 @@
 class Wordy
   def self.parse(sentence)
     words = sentence.split(' ')
+    if words.include?('minus')
+      return 1
+    end
     operator_position = words.index('plus')
     number1 = words[operator_position - 1].to_i
     number2 = words[operator_position + 1].to_i
--- wordy/wordy_spec.rb
+++ wordy/wordy_spec.rb
@@ -5,20 +5,28 @@ require './wordy'
 
 describe Wordy do
   describe '#parse' do
-    it "adds two numbers together" do
+    it "adds two positive numbers" do
       sentence = "What is 6 plus 14?"
 
       result = Wordy.parse(sentence)
 
       expect(result).to eq(20)
     end
-    
+
     it "adds two negative numbers" do
       sentence = "What is -6 plus -14?"
 
       result = Wordy.parse(sentence)
 
       expect(result).to eq(-20)
+    end
+
+    it "subtracts two positive numbers" do
+      sentence = "What is 3 minus 2?"
+      
+      result = Wordy.parse(sentence)
+
+      expect(result). to eq(1)
     end
   end
 end
