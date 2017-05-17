eebc3a05db412fbf0240bfa9d9669d89e0c1b02b
kborn <kristina.born@gmail.com>
2017-05-17 11:26:53 -0400

    refactors subtraction

--- wordy/wordy.rb
+++ wordy/wordy.rb
@@ -1,14 +1,22 @@
 class Wordy
+  # step 1: find operator and operands
+  # step 2: evaluate
+
   def self.parse(sentence)
     words = sentence.split(' ')
     if words.include?('minus')
-      return 1
-    end
-    operator_position = words.index('plus')
-    number1 = words[operator_position - 1].to_i
-    number2 = words[operator_position + 1].to_i
-    sum = number1 + number2
+      operator_position = words.index('minus')
+      number1 = words[operator_position - 1].to_i
+      number2 = words[operator_position + 1].to_i
 
-    sum
+      number1 - number2
+    else
+      operator_position = words.index('plus')
+      number1 = words[operator_position - 1].to_i
+      number2 = words[operator_position + 1].to_i
+
+      number1 + number2
+    end
+
   end
 end
